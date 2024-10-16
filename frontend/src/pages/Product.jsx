622bec477752bbc5bdffde77d0ebe38a80f9801d
Mayur Hegde <mayurhegde11@gmail.com>
2024-10-16 21:48:01 +0530

    products

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
+  const { productId } = useParams();
+  const { products } = useContext(ShopContext);
+  const [productData, setProductData] = useState(null);
+  const [image, setImage] = useState('');
 
-  const {productId} = useParams();
-  const {pro}
+  const fetchProductData = () => {
+    const product = products.find((item) => item._id === productId);
+    if (product) {
+      setProductData(product);
+      setImage(product.image[0]); // Set the first image as the default
+    }
+  };
 
+  useEffect(() => {
+    if (products.length) {
+      fetchProductData();
+    }
+  }, [productId, products]);
 
-  return (
-    <div>Product</div>
-  )
-}
+  return productData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+        <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+            {productData.image?.map((item, index) => (
+              <img
+                src={item}
+                key={index}
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
+                onClick={() => setImage(item)}
+              />
+            ))}
+          </div>
+          <div className="w-full sm:w-[80%]">
+            <img className="w-full h-auto" src={image} alt="Product" />
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
+};
 
-export default Product
+export default Product;
