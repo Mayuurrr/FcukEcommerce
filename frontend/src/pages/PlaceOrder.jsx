99b94bf39ddf6aec53d87cd9a5aed7d48b3af7c1
Mayur Hegde <mayurhegde11@gmail.com>
2024-11-30 22:07:07 +0530

    Stripe Integration

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -71,6 +71,22 @@ const PlaceOrder = () => {
           }
           break;
 
+          //Stripe
+          case 'stripe':
+            const responseStripe = await axios.post(backendURL + '/api/order/stripe',orderData,{headers:{token}})
+            console.log(responseStripe);
+            
+            if(responseStripe.data.success) {
+              const { session_url } = responseStripe.data
+              window.location.replace(session_url)
+            }
+            else {
+              toast.error(responseStripe.data.error)
+            }
+
+
+          break;
+
         default:
           break;
       }
